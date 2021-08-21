2f595ae67582c7c11d88a4d520acd50298a4d17c
nandosmc29 <89163526+nandosmc29@users.noreply.github.com>
2021-08-21 22:19:54 +0200

    Start button transition
    Added functionality to make start button transition to main screen.

--- Assets/Scripts/MenuScreen/MenuScript.cs
+++ Assets/Scripts/MenuScreen/MenuScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -26,5 +27,10 @@ public class MenuScript : MonoBehaviour
 #else
         Application.Quit(); // original code to quit Unity player
 #endif
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
     }
 }
